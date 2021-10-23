13ff1342b5c97c34ad26bb5142d9765a0a3176e6
Souvik Raj Singh <souvikrajsingh@gmail.com>
2021-10-23 14:06:21 +0530

    Update Day37

--- Day37/LC_509_Fibonacci.java
+++ Day37/LC_509_Fibonacci.java
@@ -4,7 +4,7 @@ public class LC_509_Fibonacci {
 
     public static void main(String[] args) {
 
-        System.out.println(fib(14));
+        System.out.println(fib(1));
 
     }
     public static int fib(int n) {
@@ -13,18 +13,18 @@ public class LC_509_Fibonacci {
         /*Runtime: 14 ms, faster than 8.39% of Java online submissions for Fibonacci Number.
     Memory Usage: 37.8 MB, less than 18.80% of Java online submissions for Fibonacci Number.
 
-    using recursion
+    using recursion*/
 
         if (n<2)
         {
             return n;
         }
-        return fib(n - 2) + fib(n - 1) ;*/
+        return fib(n - 2) + fib(n - 1) ;
 
         /* Runtime: 0 ms, faster than 100.00% of Java online submissions for Fibonacci Number.
         Memory Usage: 35.8 MB, less than 66.69% of Java online submissions for Fibonacci Number.*/
 
-        if(n==0)
+        /*if(n==0)
             return 0;
 
         int dp[]=new int[n+1];
@@ -34,7 +34,7 @@ public class LC_509_Fibonacci {
         for(int i=2;i<=n;i++)
             dp[i]=dp[i-1]+dp[i-2];
 
-        return dp[n];
+        return dp[n];*/
     }
 }
 
---
+++ Day40/GFG_RecursiveBubbleSort.java
@@ -0,0 +1,16 @@
+package com.souvik.Day40;
+
+//https://www.geeksforgeeks.org/recursive-bubble-sort/
+
+public class GFG_RecursiveBubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = { 1,9,3,4,7,5};
+        System.out.println(check(arr));
+    }
+
+    public static boolean check(int[] arr)
+    {
+        
+    }
+}
