c041aac5ed1d0136b2231589d1fc353e73da2973
Souvik Raj Singh <souvikrajsingh@gmail.com>
2021-10-25 09:54:59 +0530

    Update Day41

--- Day40/GFG_RecursiveBubbleSort.java
+++ Day40/GFG_RecursiveBubbleSort.java
@@ -2,15 +2,42 @@ package com.souvik.Day40;
 
 //https://www.geeksforgeeks.org/recursive-bubble-sort/
 
+import java.util.Arrays;
+
 public class GFG_RecursiveBubbleSort {
 
-    public static void main(String[] args) {
-        int[] arr = { 1,9,3,4,7,5};
-        System.out.println(check(arr));
+    static void bubbleSort(int arr[], int n)
+    {
+        // Base case
+        if (n == 1)
+            return;
+
+        // One pass of bubble sort. After
+        // this pass, the largest element
+        // is moved (or bubbled) to end.
+        for (int i=0; i<n-1; i++)
+            if (arr[i] > arr[i+1])
+            {
+                // swap arr[i], arr[i+1]
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+
+        // Largest element is fixed,
+        // recur for remaining array
+        bubbleSort(arr, n-1);
     }
 
-    public static boolean check(int[] arr)
+    // Driver Method
+    public static void main(String[] args)
     {
-        
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+
+        bubbleSort(arr, arr.length);
+
+        System.out.println("Sorted array : ");
+        System.out.println(Arrays.toString(arr));
     }
 }
+
