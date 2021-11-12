75303726e1592918526fde4a0e795200e584735b
Souvik Raj Singh <souvikrajsingh@gmail.com>
2021-11-12 18:10:51 +0530

    Update Day42

--- Day42/OOPS.java
+++ Day42/OOPS.java
@@ -1,2 +1,32 @@
-package com.souvik.Day42;public class OOPS {
+package com.souvik.Day42;
+
+public class OOPS {
+
+    public static void main(String[] args) {
+
+        int[] rno = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
+
+        Student[] students = new Student[5];
+
+        Student kunal = new Student();
+
+        kunal.rno = 13;
+        kunal.name = "Souvik Raj Singh";
+        kunal.marks = 85.6f;
+
+        System.out.println(kunal.rno);
+        System.out.println(kunal.name);
+        System.out.println(kunal.marks);
+
+    }
+
+}
+class Student
+{
+    int rno;
+    String name;
+    float marks ;
+
 }
