e4049b8e47cd14a3f203aa3c782abf0210a96ddf
Souvik Raj Singh <souvikrajsingh@gmail.com>
2021-11-15 11:50:29 +0530

    Day42

--- Day42/Greetings.java
+++ Day42/Greetings.java
@@ -1,13 +1,10 @@
 package com.souvik.Day42;
 
+import static com.souvik.Day42.Message.message;
+
 public class Greetings {
 
     public static void main(String[] args) {
-        
         message();
-        
-    }
-
-    private static void message() {
     }
 }
---
+++ Day42/Message.java
@@ -0,0 +1,13 @@
+package com.souvik.Day42;
+
+public class Message {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void message()
+    {
+        System.out.println("Hey there good morning ");
+    }
+}
